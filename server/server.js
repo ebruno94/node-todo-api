1de2ba6b63afb7500c440e0fbd32eaf54d42357e
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-02-12 13:41:21 -0800

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -24,9 +24,16 @@ app.post('/todos', (req, res) => {
 });
 
 // GET /todos/123asdas to read Todos
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  })
+});
 
 app.listen(3000, () => {
   console.log('Started on port 3000!');
-})
+});
 
 module.exports = {app};
