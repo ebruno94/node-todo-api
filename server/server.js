4a0a5087444a0a4a70dd2e75f7855881a84e6569
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-02-12 14:48:38 -0800

    Add return statements to avoid Unhandled promise rejections  during test and in future uses

--- server/server.js
+++ server/server.js
@@ -37,14 +37,14 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
   var id = req.params.id;
   if (!ObjectID.isValid(id)){
-    res.status(404).send({})
+    return res.status(404).send({})
   };
 
   Todo.findById(id).then((todo) => {
     if (!todo){
-      res.status(404).send({});
+      return res.status(404).send({});
     }
-    res.send({todo})
+    return res.send({todo})
   }, (e) => res.status(400).send({}));
 });
 
