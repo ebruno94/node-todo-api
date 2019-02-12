9648172df5b02521cfab877647cd7a00c2c9b3e6
Ernest Bruno <ernest.bruno94@yahoo.com>
2019-02-12 12:04:26 -0800

    Refactor and modulize. Move schemas into model folder and move Mongoose DB connection to its own db folder and mongoose.js file.

--- server/server.js
+++ server/server.js
@@ -1,29 +1,7 @@
-var mongoose = require('mongoose');
+var {mongoose} = require('./db/mongoose');
+var {Todo} = require('./models/todo');
+var {User} = require('./models/user');
 
-// Tell mongoose to use built in promise
-mongoose.Promise = global.Promise;
-// Tell mongoose to connect
-mongoose.connect('mongodb://localhost:27017/TodoApp');
-
-var Todo = mongoose.model('Todo', {
-  text: {
-    type: String,
-    // Value must exist if required is true.
-    required: true,
-    // minimum Length validator,
-    minlength: 1,
-    // Remove trailing and leading white spaces
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
-});
 
 // var newTodo = new Todo({
 //   text: 'Cook dinner'
@@ -49,16 +27,6 @@ newTodo2.save().then((d) => {
   console.log('Unable to save', e);
 });
 
-
-// New User Model CHALLENGE
-var User = mongoose.model('Users', {
-  email: {
-    require: true,
-    trim: true,
-    type: String,
-    minlength: 1
-  }
-})
 
 var newUser = new User({
   email: 'ernnex@gmail.com'
